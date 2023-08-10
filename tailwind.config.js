f48cd75f59b9be56211a97c8803fcbce19dc7447
Daniel Fürst <69599541+DanRantino@users.noreply.github.com>
2023-08-09 21:30:50 -0300

    inital commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        btn: {
-          background: 'hsl(var(--btn-background))',
-          'background-hover': 'hsl(var(--btn-background-hover))',
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#F6F9FC',
+          secondary: '#526475',
+          accent: '#D8E3E7',
+          neutral: '#7F92A4',
+          'base-100': '#061B2E',
+          info: '#1C85E8',
+          success: '#2CC8A7',
+          warning: '#FFC259',
+          error: '#FF8082',
         },
       },
-    },
+    ],
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+};
