da350a3a5086bc66458452f2e0cdbcb81526cd2e
Daniel Fürst <69599541+DanRantino@users.noreply.github.com>
2023-08-22 23:24:05 -0300

    feat: query para atualizar task

--- src/server/tasks/actions.ts
+++ src/server/tasks/actions.ts
@@ -1,20 +1,11 @@
 'use server';
-import { revalidatePath } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { createServerClient } from '../serverClient';
 
 export async function getTasks() {
   const supabase = createServerClient();
   revalidatePath('/');
   return await supabase.from('tasks').select('*, profiles( username  ) ');
-}
-
-export async function doneTask(id: string | undefined, done: boolean) {
-  const supabase = createServerClient();
-  if (!id) return null;
-  console.log('🚀 ~ file: actions.ts:16 ~ doneTask ~ done:', done);
-
-  const ret = await supabase.from('tasks').update({ done }).eq('id', 'f154a4f3-59fa-41d0-86e4-7aa57274f218');
-  console.log('🚀 ~ file: actions.ts:17 ~ doneTask ~ ret:', ret);
 }
 
 export async function deleteById(id: string | undefined) {
@@ -29,4 +20,17 @@ export async function getTaskById(id: string | null) {
   if (!id) return null;
   const data = (await supabase.from('tasks').select('*, profiles( username  )').eq('id', id).single()).data;
   return data;
+}
+
+export async function updateTask(
+  id: string | null,
+  task: string | null | undefined,
+  status: 'BACKLOG' | 'PENDING' | 'IN PROGRESS' | 'COMPLETED' | 'TO DO' | null | undefined,
+  username: string | null | undefined,
+) {
+  const supabase = createServerClient();
+  if (!id) return null;
+  const { data, error } = await supabase.from('profiles').select('*').eq('username', username).single();
+  if (!data) return null;
+  await supabase.from('tasks').update({ task, status, user_id: data.id }).eq('id', id);
 }
